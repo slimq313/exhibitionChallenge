67310ad10a16b187dc325a5da526244d75511cfb
Stanley Adigwe <adigwestanley@gmail.com>
2017-11-16 07:06:13 -0500

    update on exhibition challenge

--- Exhibition/ECViewController.m
+++ Exhibition/ECViewController.m
@@ -17,6 +17,11 @@
 @property (nonatomic) UIImageView *circleLarge;
 @property (nonatomic) UIImageView *circleFloaters;
 @property (nonatomic) UIImageView *thumbnail;
+@property (nonatomic) UIImageView *firstDot;
+@property (nonatomic) UIImageView *sencondDot;
+@property (nonatomic) UIImageView *thirdDot;
+@property (nonatomic) UIImageView *fourthDot;
+@property (nonatomic) UIImageView *fifthDot;
 
 @end
 
@@ -28,7 +33,6 @@
     [super loadView];
 
     // Frames
-
     NSString *videoPath = [[NSBundle mainBundle] pathForResource:@"ornaments" ofType:@"mp4"];
     NSURL *videoURL = [NSURL fileURLWithPath: videoPath];
     
@@ -75,125 +79,9 @@
     [self.view addSubview:photoCaptainAmerica];
     [photoCaptainAmerica setFrameFromSize:CGSizeMake(300.0,300.0) andParentPaddingTop:200.0 right:50.0 bottom:0.0 left:50.0 constrainSize:YES];
     
-    //gray dots
-    UIImageView *firstDot = [[UIImageView alloc] initWithFrame: CGRectMake(310, 220, 15, 15)];
-    firstDot.backgroundColor = [UIColor whiteColor];
-    firstDot.alpha = 0.20;
-    [firstDot.layer setCornerRadius: firstDot.frame.size.width/2];
-    [firstDot.layer setBorderWidth: 1];
-    [firstDot.layer setMasksToBounds: YES];
-    [self.view addSubview: firstDot];
-    
-    UIImageView *sencondDot = [[UIImageView alloc] initWithFrame: CGRectMake(270, 350, 15, 15)];
-    sencondDot.backgroundColor = [UIColor whiteColor];
-    sencondDot.alpha = 0.20;
-    [sencondDot.layer setCornerRadius: sencondDot.frame.size.width/2];
-    [sencondDot.layer setBorderWidth: 1];
-    [sencondDot.layer setMasksToBounds: YES];
-    [self.view addSubview: sencondDot];
-    
-    UIImageView *thirdDot = [[UIImageView alloc] initWithFrame: CGRectMake(230, 420, 15, 15)];
-    thirdDot.backgroundColor = [UIColor whiteColor];
-    thirdDot.alpha = 0.20;
-    [thirdDot.layer setCornerRadius: thirdDot.frame.size.width/2];
-    [thirdDot.layer setBorderWidth: 1];
-    [thirdDot.layer setMasksToBounds: YES];
-    [self.view addSubview: thirdDot];
-    
-    UIImageView *fourthDot = [[UIImageView alloc] initWithFrame: CGRectMake(130, 450, 15, 15)];
-    fourthDot.backgroundColor = [UIColor whiteColor];
-    fourthDot.alpha = 0.20;
-    [fourthDot.layer setCornerRadius: fourthDot.frame.size.width/2];
-    [fourthDot.layer setBorderWidth: 1];
-    [fourthDot.layer setMasksToBounds: YES];
-    [self.view addSubview: fourthDot];
-    
-    UIImageView *fifthDot = [[UIImageView alloc] initWithFrame: CGRectMake(155, 350, 15, 15)];
-    fifthDot.backgroundColor = [UIColor whiteColor];
-    fifthDot.alpha = 0.20;
-    [fifthDot.layer setCornerRadius: fifthDot.frame.size.width/2];
-    [fifthDot.layer setBorderWidth: 1];
-    [fifthDot.layer setMasksToBounds: YES];
-    [self.view addSubview: fifthDot];
-    
-    //line 1
-    UIBezierPath *LineOne = [UIBezierPath bezierPath];
-    [LineOne moveToPoint: CGPointMake(firstDot.center.x, firstDot.center.y)];
-    [LineOne addLineToPoint: CGPointMake(sencondDot.center.x, sencondDot.center.y)];
-    CAShapeLayer *shapeLayerOne = [CAShapeLayer layer];
-    shapeLayerOne.path = [LineOne CGPath];
-    shapeLayerOne.strokeColor = [[UIColor whiteColor] CGColor];
-    shapeLayerOne.lineWidth = 1.0;
-    shapeLayerOne.opacity = 0.20;
-    shapeLayerOne.fillColor = [[UIColor whiteColor] CGColor];
-    [self.view.layer addSublayer:shapeLayerOne];
-    
-    //line 2
-    UIBezierPath *LineTwo = [UIBezierPath bezierPath];
-    [LineTwo moveToPoint: CGPointMake(sencondDot.center.x, sencondDot.center.y)];
-    [LineTwo addLineToPoint: CGPointMake(thirdDot.center.x, thirdDot.center.y)];
-    CAShapeLayer *shapeLayerTwo = [CAShapeLayer layer];
-    shapeLayerTwo.path = [LineTwo CGPath];
-    shapeLayerTwo.strokeColor = [[UIColor whiteColor] CGColor];
-    shapeLayerTwo.lineWidth = 1.0;
-    shapeLayerTwo.opacity = 0.20;
-    shapeLayerTwo.fillColor = [[UIColor whiteColor] CGColor];
-    
-    //line 3
-    UIBezierPath *LineThree = [UIBezierPath bezierPath];
-    [LineThree moveToPoint: CGPointMake(thirdDot.center.x, thirdDot.center.y)];
-    [LineThree addLineToPoint: CGPointMake(fourthDot.center.x, fourthDot.center.y)];
-    CAShapeLayer *shapeLayerThree = [CAShapeLayer layer];
-    shapeLayerThree.path = [LineThree CGPath];
-    shapeLayerThree.strokeColor = [[UIColor whiteColor] CGColor];
-    shapeLayerThree.lineWidth = 1.0;
-    shapeLayerThree.opacity = 0.20;
-    shapeLayerThree.fillColor = [[UIColor whiteColor] CGColor];
-    
-    //line 4
-    UIBezierPath *LineFour = [UIBezierPath bezierPath];
-    [LineFour moveToPoint: CGPointMake(fourthDot.center.x, fourthDot.center.y)];
-    [LineFour addLineToPoint: CGPointMake(fifthDot.center.x, fifthDot.center.y)];
-    CAShapeLayer *shapeLayerFour = [CAShapeLayer layer];
-    shapeLayerFour.path = [LineFour CGPath];
-    shapeLayerFour.strokeColor = [[UIColor whiteColor] CGColor];
-    shapeLayerFour.lineWidth = 1.0;
-    shapeLayerFour.opacity = 0.20;
-    shapeLayerFour.fillColor = [[UIColor whiteColor] CGColor];
-    
-    //line 5
-    UIBezierPath *LineFive = [UIBezierPath bezierPath];
-    [LineFive moveToPoint: CGPointMake(thirdDot.center.x, thirdDot.center.y)];
-    [LineFive addLineToPoint: CGPointMake(fifthDot.center.x, fifthDot.center.y)];
-    CAShapeLayer *shapeLayerFive = [CAShapeLayer layer];
-    shapeLayerFive.path = [LineFive CGPath];
-    shapeLayerFive.strokeColor = [[UIColor whiteColor] CGColor];
-    shapeLayerFive.lineWidth = 1.0;
-    shapeLayerFive.opacity = 0.20;
-    shapeLayerFive.fillColor = [[UIColor whiteColor] CGColor];
-    
-    [self.view.layer addSublayer:shapeLayerOne];
-    [self.view.layer addSublayer:shapeLayerTwo];
-    [self.view.layer addSublayer:shapeLayerThree];
-    [self.view.layer addSublayer:shapeLayerFour];
-    [self.view.layer addSublayer:shapeLayerFive];
-    
-    _circleFloaters = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"circle_floaters"]];
-    [self.view addSubview: _circleFloaters];
-    _circleFloaters.center = CGPointMake(fourthDot.center.x, fourthDot.center.y);
-    
-    _thumbnail = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"thumb_ca"]];
-    _thumbnail.frame = CGRectMake(0, 0, 130.5, 130.5);
-    [_thumbnail.layer setCornerRadius: _thumbnail.frame.size.width/2];
-    [_thumbnail.layer setBorderWidth: 0];
-    [_thumbnail.layer setMasksToBounds: YES];
-    [self.view addSubview: _thumbnail];
-    _thumbnail.center = CGPointMake(fourthDot.center.x, fourthDot.center.y);
-    
-    _circleLarge = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"circle_lrg"]];
-    [self.view addSubview: _circleLarge];
-    _circleLarge.center = CGPointMake(fourthDot.center.x, fourthDot.center.y);
-    
+    [self setUpUnavailableModes];
+    [self setUpLines];
+    [self setUpCircularRings];
     [self startRotateTimer];
 }
 
@@ -206,7 +94,7 @@
     [self runSpinAnimationOnView:_circleFloaters duration: -20.0 rotations:0 repeat:0];
 }
 
-//rotate
+//rotate circular ring
 - (void) runSpinAnimationOnView:(UIView*)view duration:(CGFloat)duration rotations:(CGFloat)rotations repeat:(float)repeat {
     CABasicAnimation* rotationAnimation;
     rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
@@ -215,6 +103,135 @@
     rotationAnimation.cumulative = NO;
     rotationAnimation.repeatCount = repeat ? HUGE_VALF : 0;
     [_circleFloaters.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
+}
+
+//draw Up dots for unavailable modes
+- (void) setUpUnavailableModes {
+    //Unavailable modes (gray dots)
+    _firstDot = [[UIImageView alloc] initWithFrame: CGRectMake(310, 220, 15, 15)];
+    _firstDot.backgroundColor = [UIColor whiteColor];
+    _firstDot.alpha = 0.20;
+    [_firstDot.layer setCornerRadius: _firstDot.frame.size.width/2];
+    [_firstDot.layer setBorderWidth: 1];
+    [_firstDot.layer setMasksToBounds: YES];
+    [self.view addSubview: _firstDot];
+    
+    _sencondDot = [[UIImageView alloc] initWithFrame: CGRectMake(270, 350, 15, 15)];
+    _sencondDot.backgroundColor = [UIColor whiteColor];
+    _sencondDot.alpha = 0.20;
+    [_sencondDot.layer setCornerRadius: _sencondDot.frame.size.width/2];
+    [_sencondDot.layer setBorderWidth: 1];
+    [_sencondDot.layer setMasksToBounds: YES];
+    [self.view addSubview: _sencondDot];
+    
+    _thirdDot = [[UIImageView alloc] initWithFrame: CGRectMake(230, 420, 15, 15)];
+    _thirdDot.backgroundColor = [UIColor whiteColor];
+    _thirdDot.alpha = 0.20;
+    [_thirdDot.layer setCornerRadius: _thirdDot.frame.size.width/2];
+    [_thirdDot.layer setBorderWidth: 1];
+    [_thirdDot.layer setMasksToBounds: YES];
+    [self.view addSubview: _thirdDot];
+    
+    _fourthDot = [[UIImageView alloc] initWithFrame: CGRectMake(130, 450, 15, 15)];
+    _fourthDot.backgroundColor = [UIColor whiteColor];
+    _fourthDot.alpha = 0.20;
+    [_fourthDot.layer setCornerRadius: _fourthDot.frame.size.width/2];
+    [_fourthDot.layer setBorderWidth: 1];
+    [_fourthDot.layer setMasksToBounds: YES];
+    [self.view addSubview: _fourthDot];
+    
+    _fifthDot = [[UIImageView alloc] initWithFrame: CGRectMake(155, 350, 15, 15)];
+    _fifthDot.backgroundColor = [UIColor whiteColor];
+    _fifthDot.alpha = 0.20;
+    [_fifthDot.layer setCornerRadius: _fifthDot.frame.size.width/2];
+    [_fifthDot.layer setBorderWidth: 1];
+    [_fifthDot.layer setMasksToBounds: YES];
+    [self.view addSubview: _fifthDot];
+}
+
+//draw Up Circular Rings
+- (void) setUpCircularRings {
+    _circleFloaters = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"circle_floaters"]];
+    [self.view addSubview: _circleFloaters];
+    _circleFloaters.center = CGPointMake(_fourthDot.center.x, _fourthDot.center.y);
+    
+    _thumbnail = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"thumb_ca"]];
+    _thumbnail.frame = CGRectMake(0, 0, 130.5, 130.5);
+    [_thumbnail.layer setCornerRadius: _thumbnail.frame.size.width/2];
+    [_thumbnail.layer setBorderWidth: 0];
+    [_thumbnail.layer setMasksToBounds: YES];
+    [self.view addSubview: _thumbnail];
+    _thumbnail.center = CGPointMake(_fourthDot.center.x, _fourthDot.center.y);
+    
+    _circleLarge = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"circle_lrg"]];
+    [self.view addSubview: _circleLarge];
+    _circleLarge.center = CGPointMake(_fourthDot.center.x, _fourthDot.center.y);
+}
+
+//draw Up connected Lines
+- (void) setUpLines {
+    
+    //line 1
+    UIBezierPath *LineOne = [UIBezierPath bezierPath];
+    [LineOne moveToPoint: CGPointMake(_firstDot.center.x, _firstDot.center.y)];
+    [LineOne addLineToPoint: CGPointMake(_sencondDot.center.x, _sencondDot.center.y)];
+    CAShapeLayer *shapeLayerOne = [CAShapeLayer layer];
+    shapeLayerOne.path = [LineOne CGPath];
+    shapeLayerOne.strokeColor = [[UIColor whiteColor] CGColor];
+    shapeLayerOne.lineWidth = 1.0;
+    shapeLayerOne.opacity = 0.20;
+    shapeLayerOne.fillColor = [[UIColor whiteColor] CGColor];
+    [self.view.layer addSublayer:shapeLayerOne];
+    
+    //line 2
+    UIBezierPath *LineTwo = [UIBezierPath bezierPath];
+    [LineTwo moveToPoint: CGPointMake(_sencondDot.center.x, _sencondDot.center.y)];
+    [LineTwo addLineToPoint: CGPointMake(_thirdDot.center.x, _thirdDot.center.y)];
+    CAShapeLayer *shapeLayerTwo = [CAShapeLayer layer];
+    shapeLayerTwo.path = [LineTwo CGPath];
+    shapeLayerTwo.strokeColor = [[UIColor whiteColor] CGColor];
+    shapeLayerTwo.lineWidth = 1.0;
+    shapeLayerTwo.opacity = 0.20;
+    shapeLayerTwo.fillColor = [[UIColor whiteColor] CGColor];
+    
+    //line 3
+    UIBezierPath *LineThree = [UIBezierPath bezierPath];
+    [LineThree moveToPoint: CGPointMake(_thirdDot.center.x, _thirdDot.center.y)];
+    [LineThree addLineToPoint: CGPointMake(_fourthDot.center.x, _fourthDot.center.y)];
+    CAShapeLayer *shapeLayerThree = [CAShapeLayer layer];
+    shapeLayerThree.path = [LineThree CGPath];
+    shapeLayerThree.strokeColor = [[UIColor whiteColor] CGColor];
+    shapeLayerThree.lineWidth = 1.0;
+    shapeLayerThree.opacity = 0.20;
+    shapeLayerThree.fillColor = [[UIColor whiteColor] CGColor];
+    
+    //line 4
+    UIBezierPath *LineFour = [UIBezierPath bezierPath];
+    [LineFour moveToPoint: CGPointMake(_fourthDot.center.x, _fourthDot.center.y)];
+    [LineFour addLineToPoint: CGPointMake(_fifthDot.center.x, _fifthDot.center.y)];
+    CAShapeLayer *shapeLayerFour = [CAShapeLayer layer];
+    shapeLayerFour.path = [LineFour CGPath];
+    shapeLayerFour.strokeColor = [[UIColor whiteColor] CGColor];
+    shapeLayerFour.lineWidth = 1.0;
+    shapeLayerFour.opacity = 0.20;
+    shapeLayerFour.fillColor = [[UIColor whiteColor] CGColor];
+    
+    //line 5
+    UIBezierPath *LineFive = [UIBezierPath bezierPath];
+    [LineFive moveToPoint: CGPointMake(_thirdDot.center.x, _thirdDot.center.y)];
+    [LineFive addLineToPoint: CGPointMake(_fifthDot.center.x, _fifthDot.center.y)];
+    CAShapeLayer *shapeLayerFive = [CAShapeLayer layer];
+    shapeLayerFive.path = [LineFive CGPath];
+    shapeLayerFive.strokeColor = [[UIColor whiteColor] CGColor];
+    shapeLayerFive.lineWidth = 1.0;
+    shapeLayerFive.opacity = 0.20;
+    shapeLayerFive.fillColor = [[UIColor whiteColor] CGColor];
+    
+    [self.view.layer addSublayer:shapeLayerOne];
+    [self.view.layer addSublayer:shapeLayerTwo];
+    [self.view.layer addSublayer:shapeLayerThree];
+    [self.view.layer addSublayer:shapeLayerFour];
+    [self.view.layer addSublayer:shapeLayerFive];
 }
 
 @end
